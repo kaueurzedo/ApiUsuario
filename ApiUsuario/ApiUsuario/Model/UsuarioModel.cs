0d4bffabad28e84c360465b81c03165af677374e
Kaue Ignacio de Urzedo <kaueurzedo13@hotmail.com>
2023-02-18 15:36:49 -0300

    Api Usuario
    Api de usuario de consulta, alteração de dados, cadastro de dado e exclusão.

---
+++ ApiUsuario/ApiUsuario/Model/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiUsuario.Model
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+
+        public string? Email { get; set;}
+
+
+
+
+    }
+}
